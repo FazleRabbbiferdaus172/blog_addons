d5294660bf745df70a458f4922429f42bc3f828b
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2023-07-07 19:28:02 +0600

    Starting template folder for bus_notification

---
+++ bus_notification/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bus Notification Demo",
+
+    'summary': """Bus Notification Demo.""",
+
+    'description': """
+        Bus Notification Demo.
+    """,
+
+    'author': "Fazle Rabbi Ferdaus",
+    'category': 'Uncategorized',
+    'version': '15.0.0.1',
+
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+}
